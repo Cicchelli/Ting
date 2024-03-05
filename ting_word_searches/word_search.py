86f90279fccd4a0cb284cc9c34b18d61292e1865
Stephan <cicchelli.stephan@gmail.com>
2024-03-05 12:23:40 -0300

    Req8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -24,4 +24,27 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for file_index in range(len(instance)):
+        file = instance.search(file_index)
+        occurrences = []
+
+        for line_index, line_content in enumerate(
+            file["linhas_do_arquivo"], start=1
+        ):
+            if word.lower() in line_content.lower():
+                occurrences.append(
+                    {"linha": line_index, "conteudo": line_content}
+                )
+
+        if occurrences:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return result
